69a2b2cf189e323e0063be5844b555ca5da68281
Kane Walter <kwal0203@gmail.com>
2019-01-01 15:51:42 +1100

    Image processing complete

--- DataGenerator.py
+++ DataGenerator.py
@@ -1,11 +1,61 @@
+import os
+
+from random import randint
+
 # Class responsible for: calculating where to paste detection images on
 # background, pasting images on backgrounds, formatting and outputting CSV
 # files
 
 class DataGenerator:
-    def __init__(self, image_set, background_set):
+    def __init__(self, image_set, background_set, num_samples, cutoff):
         self.image_set = image_set
         self.background_set = background_set
+        self.num_samples = num_samples
+        self.detail_list = []
+        self.csv_lines = []
+        self.cutoff = cutoff * num_samples
 
     def process_images(self):
         print("----- PROCESSING IMAGES -----")
+        sample_no = 1
+
+        train_dir = "output/train/"
+        test_dir = "output/test/"
+        if not os.path.exists(train_dir):
+            os.makedirs(train_dir)
+        if not os.path.exists(test_dir):
+            os.makedirs(test_dir)
+
+        for background in self.background_set.background_images:
+            for i in range(0, self.num_samples):
+                tmp_back = background.background_image.copy()
+                new_name = str(sample_no) + ".png"
+                for image in self.image_set.detection_images:
+                    r_width = tmp_back.width - image.width
+                    r_height = tmp_back.height - image.height
+
+                    width_offset = randint(0, r_width)
+                    height_offset = randint(0, r_height)
+                    offset = (width_offset, height_offset)
+
+                    # Place card in offset position
+                    tmp_back.paste(image.detection_image, offset)
+
+                    # x_min, y_min, x_max, y_max
+                    x_min = offset[0]
+                    y_min = offset[1]
+                    x_max = x_min + image.width
+                    y_max = y_min + image.height
+                    self.detail_list.append((new_name, tmp_back.width,
+                                             tmp_back.height, 'XXX', x_min,
+                                             y_min, x_max, y_max))
+
+                # Name and save the new image (background image remains
+                # unchanged)
+                print("Processing Sample ", sample_no)
+
+                if sample_no <= self.cutoff:
+                    tmp_back.save("output/train/" + new_name)
+                else:
+                    tmp_back.save("output/test/" + new_name)
+                sample_no += 1
